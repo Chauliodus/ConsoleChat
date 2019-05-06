0ba33e6f1a44e1666a15fe5db37d23150fe87db8
Chauliodus <avshved@ngs.ru>
2019-05-06 15:56:39 +0700

    The program accepts as input the path to the file in which the chat will be conducted; the number of customers who will write to the chat; the delay in seconds for each client with which he will send a message to the chat; line for each client, which he will write to the chat.
    The task of the program is to write to the specified file strings
    received from customers in the format “[current time accurate to a
    second]; [customer number]: [message text] ".

--- main.cpp
+++ main.cpp
@@ -1,8 +1,84 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+
+std::mutex mtx;
+
+void chat(const std::string& path, int index, const std::string& message, const int delay)
+{
+    while(1)
+    {
+        mtx.lock();
+
+        std::fstream file(path, std::ios_base::app);
+        if (!file.is_open())
+        {
+            std::cerr << "cannot open file " << path;
+            exit(0);
+        }
+        char buffer[80];
+        time_t seconds = time(NULL);
+        tm *timeinfo = localtime(&seconds);
+        char* format = "%I:%M:%S";
+        strftime(buffer, 80, format, timeinfo);
+        file << "[" << buffer << "]; " << index << ": " << message << std::endl;
+        std::cout << "[" << buffer << "]; " << index << ": " << message << std::endl;
+        file.close();
+
+        mtx.unlock();
+
+        std::this_thread::sleep_for(std::chrono::seconds(delay));
+    }
+}
+
+void inputFcn(std::string& path, int& number)
+{
+    std::cout << "Input path: ";
+    std::cin >> path;
+    std::cout << "Input number of clients: ";
+    std::cin >> number;
+}
+
+void inputFcn2(int index, std::string& message, int& delay)
+{
+    std::cout << index + 1 << " message: ";
+    std::cin >> message;
+    std::cout << index + 1 << " delay: ";
+    std::cin >> delay;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    std::string path; // = "C://Qt//Projects//Chat//chat.txt";
+    int numberOfClients;
+    inputFcn(path, numberOfClients);
+
+    std::vector<std::string> messages;
+    std::vector<int> delays;
+    std::vector<std::thread> threads;
+    std::string message;
+    int delay;
+
+    for(int i = 0; i < numberOfClients; i++)
+    {
+        inputFcn2(i, message, delay);
+        messages.push_back(message);
+        delays.push_back(delay);
+    }
+
+    for(int i = 0; i < numberOfClients; i++)
+    {
+        threads.push_back(std::thread(chat, path, i + 1, messages[i], delays[i]));
+        //threads[i].join();
+    }
+
+    messages.clear();
+    delays.clear();
 
     return a.exec();
 }
